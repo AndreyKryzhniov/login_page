d3c7421c569d28eb9859e1f2df8c96edc23d2421
AndreyKryzhniov <andreykryzhniov@gmail.com>
2020-01-17 21:14:05 +0300

    Add BLL

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Login.module.css';
 import {NavLink} from "react-router-dom";
+import {putLoginTC} from '../redux/loginReducer'
+import {useDispatch} from 'react-redux';
 
 
 
 const Login: React.FC = () => {
 
+  let [email, setLoginState] = useState('')
+  let [password, setPasswordState] = useState('')
+  let [rememberMe, setValueState] = useState(false)
+  let dispatch = useDispatch()
+
+  let sendData = () => {
+    dispatch(putLoginTC(email, password, rememberMe))
+  }
+
+
   return (
     <div className={s.login}>
       <span>sing in</span>
-      <input/>
-      <input/>
+      <input value={email} onChange={(e) => setLoginState(e.target.value)}/>
+      <input value={password} onChange={(e) => setPasswordState(e.target.value)} type={'password'}/>
       <NavLink to={'/password_recover'}>Forgot password?</NavLink>
-      <input type={'checkbox'} placeholder={'Remember Me'}/>
-      <button>Sing In</button>
+      <input type={'checkbox'} placeholder={'Remember Me'} onChange={() => setValueState(true)} checked={rememberMe}/>
+      <button onClick={sendData}>Sing In</button>
       <NavLink to={'/registration'}>Registration</NavLink>
     </div>
   );
 }
 
-export default Login;
+
+
+
+
+
+export default Login
--- src/redux/loginReducer.ts
+++ src/redux/loginReducer.ts
@@ -1,20 +1,51 @@
-const ADD_LOGIN = 'ADD_LOGIN'
-const ADD_PASSWORD = 'ADD_PASSWORD'
+// const ADD_LOGIN = 'ADD_LOGIN'
+// const ADD_PASSWORD = 'ADD_PASSWORD'
+// const REMEMBER_ME = 'REMEMBER_ME'
+import {loginApi} from '../api/apiLogin'
+import {Dispatch} from "redux";
+
+
+
+const LOGIN = 'LOGIN'
 
 interface IState {
-    value: string
+    email: string,
+    password: string,
+    rememberMe: boolean
 }
 
 interface IAction {
-    type: typeof ADD_LOGIN | typeof ADD_PASSWORD
+    type: typeof LOGIN
+    email: string
+    password: string
+    rememberMe: boolean
+    // type: typeof ADD_LOGIN | typeof ADD_PASSWORD | typeof REMEMBER_ME
 }
 
-const initialState = {
-    value: ''
+const initialState: IState = {
+    email: '',
+    password: '',
+    rememberMe: false
 }
 
-export const reducerLogin = (state = initialState, action: IAction): IState => {
-    return state
+export const reducerLogin = (state: IState = initialState, action: IAction): IState => {
+    switch (action.type) {
+        case LOGIN: {
+            return {
+                ...state, email: action.email, password: action.password, rememberMe: action.rememberMe
+            }
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+let putValueAC = (email: string, password: string, rememberMe: boolean) => ({type: LOGIN, email, password, rememberMe})
+
+export const putLoginTC = (email: string, password: string, rememberMe: boolean) => async (dispatch: Dispatch) => {
+    let response = await loginApi.putLogin(email, password, rememberMe)
+    dispatch(putValueAC(response.data.email, response.data.password, response.data.rememberMe))
 }
 
 export default reducerLogin
